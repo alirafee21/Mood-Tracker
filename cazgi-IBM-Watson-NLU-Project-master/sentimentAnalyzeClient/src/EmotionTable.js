b815d9d1e6927016c696c9c5d89480011d566b98
iceiscold <iceiscold@gmail.com>
2022-05-27 03:20:40 -0400

    initial template formatted

---
+++ cazgi-IBM-Watson-NLU-Project-master/sentimentAnalyzeClient/src/EmotionTable.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './bootstrap.min.css';
+
+class EmotionTable extends React.Component {
+    render() {
+      //Returns the emotions as an HTML table
+      return (  
+        <div>
+          <p></p>
+          
+          <table className="table table-bordered">
+            <tbody>
+            {
+              
+              Object.entries(this.props.emotions).map(function(mapentry) {
+                return (
+                  <tr>
+                  <td>{mapentry[0]}</td>
+                  <td>{mapentry[1]}</td>
+                  </tr>
+                )
+              })
+            }
+            </tbody>
+          </table>
+          </div>
+          );
+        }
+    
+}
+export default EmotionTable;
